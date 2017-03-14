e2afb4d2f5ae99a0f8b56ce72e8ef1c662f1374f
Egor Duda <egor.duda@gmail.com>
2017-03-14 08:28:28 +0000

    Allow multiple different file upload inputs on the same page

--- ViewController/public_html/resources/js/upload-handler.js
+++ ViewController/public_html/resources/js/upload-handler.js
@@ -5,6 +5,7 @@ function onLoad (event) {
         var i;
         try {
             var formData = new FormData();
+            if (formData == null) { throw 'undefined'; }
             for (i = 0; i < newSubforms.length; i++) { newSubforms[i].style.display = "inline"; }
         } catch (formdata_error) {
             for (i = 0; i < legacySubforms.length; i++) { legacySubforms[i].style.display = "inline"; }
@@ -25,8 +26,8 @@ function handleUpload (event) {
 
 function doUpload (component) {
     var contextPath = component.getProperty ('contextPath');
-    var fileSelect = document.getElementById('upload');
-    var progressBar = document.getElementById('progress-bar');
+    var fileSelect = component.getParent().getPeer().getDomNode().querySelector('.upload-files');
+    var progressBar = component.getParent().getPeer().getDomNode().querySelector('.upload-progress');
     var files = fileSelect.files;
     var formData = new FormData();
     for (var i = 0; i < files.length; i++) {
